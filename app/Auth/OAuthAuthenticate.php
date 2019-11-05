be5bc2c79f090b07fb87cd34c6268b17d47c88c1
Leandro Andreaci <leandroandreaci@hotmail.com>
2019-11-05 16:30:08 -0300

    add connection to OAuthAuthenticate

--- app/Auth/OAuthAuthenticate.php
+++ app/Auth/OAuthAuthenticate.php
@@ -5,19 +5,43 @@ namespace App\Auth;
 
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Session;
 
 class OAuthAuthenticate
 {
 
     public function attempt(Request $request): bool
     {
-        return $this->send($request);
+        return $this->send($request) ;
+    }
+
+    public function connection()
+    {
+        return $this->user()->schema->name;
+    }
+
+    public function user()
+    {
+        $client = new \GuzzleHttp\Client();
+
+        $response = $client->get(env('AUTH_SERVER') . '/api/user/client',  [
+            'headers' => [
+                'Authorization' => Cache::get('TOKEN'),
+            ]
+        ] );
+
+        return json_decode($response->getBody());
     }
 
     private function send(Request $request): bool
     {
 
         $client = new \GuzzleHttp\Client();
+
+        Cache::store('file')->put('TOKEN', $request->header('authorization'));
+
         $url = env( 'AUTH_SERVER');
 
         try
@@ -27,6 +51,7 @@ class OAuthAuthenticate
                     'Authorization' => $request->header('authorization'),
                 ]
             ]);
+
             return $response->getStatusCode() == 200;
         }catch (\Exception $exception)
         {
